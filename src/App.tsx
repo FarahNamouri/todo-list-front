587dea9b0aec0a2706c5149f115ce5ede494c80f
Farah <farahjoy85@gmail.com>
2023-03-26 18:23:53 +0100

    front before axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Container, FormControl, Input, Table, TableContainer, Tbody, Th, Thead, Tr } from "@chakra-ui/react";
+import { useState } from "react";
+import { AiOutlineSearch, AiOutlineAppstoreAdd } from "react-icons/ai";
+import Line from "./Components/Line";
+// interface Tasks {
+//   taskname: string;
+//   description: string;
+// }
 
 function App() {
+  // const [tasks, setTasks] = useState<Tasks[]>([]);
+  const [query, setQuery] = useState("");
+
+  const onChangeHandler = (e: any) => {
+    setQuery(e.target.value);
+  };
+
+  // const SearchHandler = () => {
+  //   Search(query);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Container maxW={"full"} p="5" fontSize={"25px"}>
+        <Box rounded="lg" boxShadow="base" p="4">
+          <Box mt="2" gap={"2"} mb="4" display={"flex"}>
+            <FormControl>
+              <Input type="text" onChange={onChangeHandler} />
+            </FormControl>
+          </Box>
+        </Box>
+
+        <Box >
+        <Button
+              leftIcon={<AiOutlineAppstoreAdd />}
+              colorScheme="green"
+              variant="outline"
+              maxW="300px"
+              minW="150px"
+              mt="2"
+              m="4"
+            >
+              Add New Task
+            </Button>
+            <Button
+              leftIcon={<AiOutlineSearch />}
+              colorScheme="green"
+              variant="outline"
+              maxW="300px"
+              minW="150px"
+              mt="2"
+              m="4"
+
+            >
+              {" "}
+              Search
+            </Button>
+        </Box>
+
+        <Box
+          mt="5"
+          rounded="lg"
+          boxShadow="base"
+          mb="6"
+          width="50rem"
+          marginLeft="20rem"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box p="4" justifyContent="space-between" mb="2">
+            Tasks To Do
+          </Box>
+
+          <TableContainer>
+            <Table variant="simple" colorScheme="black" rounded="lg" size="sm">
+              <Thead>
+                <Tr>
+                  <Th>Task Name</Th>
+                  <Th>Description</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Line />
+                <Line />
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+
+
+        
+
+      </Container>
+    </>
   );
 }
 
---
+++ src/Components/Line.tsx
@@ -0,0 +1,31 @@
+import { Box, Button, Td, Tr } from "@chakra-ui/react";
+// import { useState } from "react";
+import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
+
+// type taskProps = {
+//     taskname: string;
+//     description: string;
+//   };
+
+const Line = () => {
+    // const [tasks, setTasks] = useState([]);
+    return (
+        <Tr>
+            <Td>taskname</Td>
+            <Td>description </Td>
+            <Td>
+            <Box display="flex" gap="1">
+          <Button colorScheme="blue">
+             <AiOutlineEdit/>
+          </Button>
+          <Button colorScheme="red" >
+            <AiOutlineDelete />
+          </Button>
+        </Box>
+        </Td>
+        </Tr>
+    );
+
+};
+
+export default Line;
